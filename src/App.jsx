565ac2a8f8c3c22d9f65be7099f65da76d42d8c0
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2023-07-24 10:58:09 +0200

    create map and markers

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import { Icon } from "leaflet";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const customIcon = new Icon({
+    iconUrl: "https://cdn-icons-png.flaticon.com/128/684/684908.png",
+    iconSize: [38, 38],
+  });
+
+  // markers
+  const markers = [
+    {
+      geocode: [52.2572312, 20.962743],
+      popUp: {
+        name: "DEIMOS SOFTWARE MARCIN ZIEMEK",
+        street: "Bolkowska 1/32",
+        city: "01-466 Warszawa",
+      },
+    },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MapContainer center={[52.2297, 21.0122]} zoom={12}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {markers.map((marker, index) => (
+          <Marker key={index} position={marker.geocode} icon={customIcon}>
+            <Popup>
+              <h5>{marker.popUp.name}</h5>
+              <p>{marker.popUp.street}</p>
+              <p>{marker.popUp.city}</p>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
